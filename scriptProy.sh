b89a4916bb728d11ccf82b96cd1e1dff2737a11a
AlanHiguera <alanhiguera144@gmail.com>
2025-11-25 20:24:05 -0300

    script done

--- scriptProy.sh
+++ scriptProy.sh
@@ -10,14 +10,14 @@ INPUT=entrada7.txt
 
 # Compilar antes de ejecutar (si ya está compilado puedes comentar esta línea)
 echo "Compilando $SRC -> $BIN"
-$CC -Wall $SRC -o "$BIN" -lm -lgmp || { echo "Compilación fallida"; exit 1; }
+$CC -O3 -Wall $SRC -o "$BIN" -lm -lgmp || { echo "Compilación fallida"; exit 1; }
 
 # Si quieres perfilar con gprof, compila con -pg en su lugar:
 # $CC -pg -g -O2 $SRC -o "$BIN" -lm -lgmp
 
-for n in 3 4 5 6 7 8; do
+for n in 1 2 3 4 5 6 7 8 9 10 11; do
   echo "=== n=$n ==="
-  OUT="outputPFSQAT${n}.txt"
-  echo "n_temporal=$n" > "$OUT"
-  $BIN $n < "$INPUT" >> "$OUT" 2> "err_n${n}.txt"
+  OUT="outputPFSQAT$.txt"
+  echo "tamaño: $n" >> "$OUT"
+  $BIN $n < "$INPUT" >> "$OUT"
 done
